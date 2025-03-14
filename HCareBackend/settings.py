261b58efbe144aab3616be6957099e6b1d46d957
wasihun-code <wasihunageru@gmail.com>
2025-03-14 14:02:02 +0530

    Migrated databse from sqlite to postgresql

--- HCareBackend/settings.py
+++ HCareBackend/settings.py
@@ -96,9 +96,17 @@ APPEND_SLASH = False
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'administrator',
+        'NAME': 'healthcare',
+        'PASSWORD': 'health-admin-123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
